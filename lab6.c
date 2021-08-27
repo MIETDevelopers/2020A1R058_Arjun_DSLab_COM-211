e61aac5c0beecc63ba8b01463ff9672a53f08160
Arjun Charak <75723799+ArjunCharak@users.noreply.github.com>
2021-08-27 20:52:30 +0530

    Update lab6.c

--- lab6.c
+++ lab6.c
@@ -87,10 +87,10 @@ void printQueue(struct Queue *queue){
 	if (noOfItems(queue) != 0)
 	{
 		do
-	    {
-	        printf("Array[%d]= %d ", count, queue->array[count]);
-	        count = (count + 1) % (queue->maxCapacity);
-	    } while (count != (queue->rear + 1) % queue->maxCapacity);
+	    	{
+	        	printf("Array[%d]= %d ", count, queue->array[count]);
+	        	count = (count + 1) % (queue->maxCapacity);
+	    	} while (count != (queue->rear + 1) % queue->maxCapacity);
 		printf("\n");
 	}
 }
@@ -109,48 +109,48 @@ int main()
 	// dynamically allocating the memory of queue
 	queueA.array = (int *)malloc(queueA.maxCapacity*sizeof(int));
 	do
-    {
-    	printf("\nChoose an operation\n");
-      	printf("1.Enqueue into the Queue\n");
-      	printf("2.Dequeue from the Queue\n");
-      	printf("3.Display the details of Queue\n");
-      	printf("4.Display the Queue\n");
-      	printf("5.Exit\n");
-      	scanf("%d", &operation);
-      	switch (operation){
-      		case 1:
-            	printf("\nEnter the value to be enqueued\n");
-        		scanf("%d", &item);
-        		enQueue(&queueA, item);			// calling enQueue function
-        		break;
-         	case 2:
-            	value = deQueue(&queueA);		// calling deQueue function
-            	if (value == INT_MIN)
-            	{
-                	printf("\nQueue is empty\n");
-            	} else{
-                	printf("\n%d is removed", value);
-            	}
-            	break;
-         	case 3:
-            	printf("\nDetails of the Queue :\n");
-            	printQueueDetails(&queueA);		// calling printQueueDetails function
-            	printf("\nTotal number of elements in queue = %d \n", noOfItems(&queueA));
-            	break;
-         	case 4:
-            	printf("\nDisplaying the entire queue :\n");
-            	printQueue(&queueA);			// calling printQueue function
-            	printf("\nTotal number of elements in queue = %d \n", noOfItems(&queueA));
-            	break;
-         	case 5:
-            	printf("\nPress Enter to exit\n");
-            	exit(0);
-            	break;
-            default:
-        		printf("\nWrong operation\n");
-        		break;
-        }
-    }
-    while (1);
-    return 0;
+    	{
+		printf("\nChoose an operation\n");
+		printf("1.Enqueue into the Queue\n");
+		printf("2.Dequeue from the Queue\n");
+		printf("3.Display the details of Queue\n");
+		printf("4.Display the Queue\n");
+		printf("5.Exit\n");
+		scanf("%d", &operation);
+		switch (operation){
+			case 1:
+				printf("\nEnter the value to be enqueued\n");
+				scanf("%d", &item);
+				enQueue(&queueA, item);			// calling enQueue function
+				break;
+			case 2:
+				value = deQueue(&queueA);		// calling deQueue function
+				if (value == INT_MIN)
+				{
+					printf("\nQueue is empty\n");
+				} else{
+					printf("\n%d is removed", value);
+				}
+				break;
+			case 3:
+				printf("\nDetails of the Queue :\n");
+				printQueueDetails(&queueA);		// calling printQueueDetails function
+				printf("\nTotal number of elements in queue = %d \n", noOfItems(&queueA));
+				break;
+			case 4:
+				printf("\nDisplaying the entire queue :\n");
+				printQueue(&queueA);			// calling printQueue function
+				printf("\nTotal number of elements in queue = %d \n", noOfItems(&queueA));
+				break;
+			case 5:
+				printf("\nPress Enter to exit\n");
+				exit(0);
+				break;
+		    	default:
+				printf("\nWrong operation\n");
+				break;
+        	}
+	}
+	while (1);
+	return 0;
 }
